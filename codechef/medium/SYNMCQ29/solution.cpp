d3bddd256f8d5d7ba37050e6b597acf380063850
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-08-02 11:02:41 +0530

    ✅ SYNMCQ29 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-02

---
+++ codechef/medium/SYNMCQ29/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int a;
+  int b;
+  cin >> a;
+  cin >> b;
+  if (a >= b) {
+    cout << "Bravo!" << endl;
+  } 
+  else {
+    cout << "Try Again" << endl;
+  }
+
+  
+}

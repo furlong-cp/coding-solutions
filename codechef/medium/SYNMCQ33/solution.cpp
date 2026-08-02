04d27f410263b1ad48ac0f298b754eb7ca8e925a
Ganesh Shanbhag <ganeshshanbhag.cp@gmail.com>
2026-08-02 11:05:53 +0530

    ✅ SYNMCQ33 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-02

---
+++ codechef/medium/SYNMCQ33/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int z;
+  int x;
+  int c;
+  cin >> z >>x>>c;
+  if (c > x || c > z ) {
+    cout << "PASS" << endl;
+  } 
+  else {
+    cout << "FAIL" << endl;
+  }
+}
